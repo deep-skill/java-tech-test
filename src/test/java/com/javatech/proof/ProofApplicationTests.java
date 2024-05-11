ac821ddbae9d909d12cb4d6b974d25544e87e1e2
Jean Pierre Mandujano G <jeanpierre.mandujano@gmail.com>
2024-05-11 12:47:22 -0500

    Empty Spring Boot project created

---
+++ src/test/java/com/javatech/proof/ProofApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatech.proof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProofApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
